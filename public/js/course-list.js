594c5701eba08ad0952cbbebd905a6c3834cf901
zhangsan <zhangsan@itcast.cn>
2017-09-25 09:15:01 +0800

    完成课时列表展示功能

--- public/js/course-list.js
+++ public/js/course-list.js
@@ -1,4 +1,16 @@
 define(['jquery','template','util'], function ($,template,util) {
     //高亮显示
     util.setMenu(location.pathname);
+
+    //获取所有数据
+    $.ajax({
+        type:'get',
+        url:'/api/course',
+        dataType:'json',
+        success: function (data) {
+            var html=template('courseTpl',{list:data.result});
+            $("#courseInfo").html(html);
+        }
+
+    })
 });
